b9825f2bfed27c07815c8cc71a2aa3cf69394475
Eli  Baird <elibaird1@gmail.com>
2017-05-05 22:28:03 -0600

    making my campaign base.

---
+++ src/CampaignServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Fuzzybaird\Campaign;
+
+use Illuminate\Support\ServiceProvider;
+
+class CampaignServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
